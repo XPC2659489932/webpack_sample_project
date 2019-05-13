c6d3bb78403ae4363cc57bd32cd3d52f1579fcc3
XPC2659489932 <2659489932@qq.com>
2019-05-13 15:59:44 +0800

    修改提交

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,35 @@ module.exports = {
 		path: __dirname+"/public",//打包后文件存放的地方 
 		filename:"bundle.js"//打包后输出文件的文件名
 	},
-
+    
 	devServer: {
 		contentBase: "./public",//本地服务器所加载的页面所在的目录
 		historyApiFallback: true,//不跳转
 		inline: true//实时刷新
+	},
+
+	module: {
+		rules: [
+		{
+			test: /(\.jsx|\.js)$/,
+			use: {
+				loader: "babel-loader"
+			},
+			exclude: /node_modules/
+		},
+		{
+			test: /\.css$/,
+			use: [
+			{
+				loader: "style-loader"
+			},{
+				loader: "css-loader"
+			}
+			]
+		}
+
+	    ]
 	}
-}
+};
 
 //__dirname是node.js的全局变量，它指向当前执行脚本所在的目录
